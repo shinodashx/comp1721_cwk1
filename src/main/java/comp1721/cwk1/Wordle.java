bf9f113e0215ccc6512df3557b0d0ae3921c54d8
shinodashx <1323837835@qq.com>
2022-04-19 15:45:15 +0800

    Add the support to accessible person. v 3.3.2

--- src/main/java/comp1721/cwk1/Wordle.java
+++ src/main/java/comp1721/cwk1/Wordle.java
@@ -9,16 +9,26 @@ import java.io.IOException;
 public class Wordle {
     public static void main(String[] args) throws IOException {
         Game game;
-
         if (args.length > 0) {
             // Player wants to specify the game
-            game = new Game(Integer.parseInt(args[0]), "data/words.txt");
+            int flag = 0;
+            for (String arg : args) {
+                if(arg.equals("-a")) {
+                    flag = 1;
+                }
+            }
+            if (flag == 1) {
+                game = new Game("data/words.txt");
+                game.getacs(1);
+            } else {
+                game = new Game(Integer.parseInt(args[0]), "data/words.txt");
+            }
         } else {
             // Play today's game
             game = new Game("data/words.txt");
         }
-
         game.play();
         game.save("build/lastgame.txt");
     }
+
 }
